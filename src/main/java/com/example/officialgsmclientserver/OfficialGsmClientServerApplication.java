59d5b8b83e71b344b47f138514e56a36e1b0143c
jangwooooo <s22018@gsm.hs.kr>
2023-03-31 12:32:37 +0900

    Initial commit

---
+++ src/main/java/com/example/officialgsmclientserver/OfficialGsmClientServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.officialgsmclientserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficialGsmClientServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficialGsmClientServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/officialgsmclientserver/OfficialGsmClientServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.officialgsmclientserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficialGsmClientServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
