6044694a92c5d8a1a48d5a7fb588a0ef9aea0483
jangwooooo <s22018@gsm.hs.kr>
2023-04-15 20:05:21 +0900

    Add Post extends BaseTimeEntity

--- src/main/java/com/example/officialgsmclientserver/domain/board/entity/post/Post.java
+++ src/main/java/com/example/officialgsmclientserver/domain/board/entity/post/Post.java
@@ -2,6 +2,7 @@ package com.example.officialgsmclientserver.domain.board.entity.post;
 
 import com.example.officialgsmclientserver.domain.board.entity.file.File;
 import com.example.officialgsmclientserver.domain.user.entity.User;
+import com.example.officialgsmclientserver.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,7 +17,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Post {
+public class Post extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
