edb86811d624f1bf3c1735b0c90f2f17c610dc54
jangwooooo <s22018@gsm.hs.kr>
2023-04-15 21:10:20 +0900

    Add findPost method

--- src/main/java/com/example/officialgsmclientserver/domain/board/service/BoardService.java
+++ src/main/java/com/example/officialgsmclientserver/domain/board/service/BoardService.java
@@ -1,14 +1,17 @@
 package com.example.officialgsmclientserver.domain.board.service;
 
+import com.example.officialgsmclientserver.domain.board.dto.response.PostDetailResponse;
 import com.example.officialgsmclientserver.domain.board.dto.response.PostListResponse;
 import com.example.officialgsmclientserver.domain.board.entity.post.Category;
 import com.example.officialgsmclientserver.domain.board.entity.post.Post;
 import com.example.officialgsmclientserver.domain.board.repository.PostRepository;
+import com.example.officialgsmclientserver.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,4 +27,10 @@ public class BoardService {
         return posts.map(PostListResponse::from);
     }
 
+    public PostDetailResponse findPost(Long postSeq) {
+        Post post = postRepository.findById(postSeq)
+                .orElseThrow(() -> new CustomException("게시글 세부 조회 과정에서 게시글을 찾지 못하였습니다.", HttpStatus.NOT_FOUND));
+
+        return PostDetailResponse.from(post);
+    }
 }
