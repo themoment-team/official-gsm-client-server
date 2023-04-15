0f6ce36d312479b988a26ac07879c39e77d5ecf8
jangwooooo <s22018@gsm.hs.kr>
2023-04-15 20:07:17 +0900

    Add BoardController

---
+++ src/main/java/com/example/officialgsmclientserver/domain/board/controller/BoardController.java
@@ -0,0 +1,25 @@
+package com.example.officialgsmclientserver.domain.board.controller;
+
+import com.example.officialgsmclientserver.domain.board.dto.response.PostListResponse;
+import com.example.officialgsmclientserver.domain.board.entity.post.Category;
+import com.example.officialgsmclientserver.domain.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping
+    public ResponseEntity<Page<PostListResponse>> postList(@RequestParam int pageNumber, @RequestParam Category category) {
+        Page<PostListResponse> postList = boardService.findPosts(pageNumber, category);
+        return new ResponseEntity<>(postList, HttpStatus.OK);
+    }
+}
