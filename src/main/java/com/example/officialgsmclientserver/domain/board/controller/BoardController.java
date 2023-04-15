b9a2a76355cf34d1ea4adc35c9b78270f585f628
jangwooooo <s22018@gsm.hs.kr>
2023-04-15 21:10:53 +0900

    Add postDetail method

--- src/main/java/com/example/officialgsmclientserver/domain/board/controller/BoardController.java
+++ src/main/java/com/example/officialgsmclientserver/domain/board/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.example.officialgsmclientserver.domain.board.controller;
 
+import com.example.officialgsmclientserver.domain.board.dto.response.PostDetailResponse;
 import com.example.officialgsmclientserver.domain.board.dto.response.PostListResponse;
 import com.example.officialgsmclientserver.domain.board.entity.post.Category;
 import com.example.officialgsmclientserver.domain.board.service.BoardService;
@@ -8,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,11 @@ public class BoardController {
     public ResponseEntity<Page<PostListResponse>> postList(@RequestParam int pageNumber, @RequestParam Category category) {
         Page<PostListResponse> postList = boardService.findPosts(pageNumber, category);
         return new ResponseEntity<>(postList, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<PostDetailResponse> postDetail(@PathVariable Long postSeq) {
+        PostDetailResponse postDetail = boardService.findPost(postSeq);
+        return new ResponseEntity<>(postDetail, HttpStatus.OK);
     }
 }
