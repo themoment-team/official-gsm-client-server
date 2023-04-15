865d49bb0dad14ac02099c2616cfe5f252dfe701
jangwooooo <s22018@gsm.hs.kr>
2023-04-15 20:06:09 +0900

    Add findAllByCategory

--- src/main/java/com/example/officialgsmclientserver/domain/board/repository/PostRepository.java
+++ src/main/java/com/example/officialgsmclientserver/domain/board/repository/PostRepository.java
@@ -1,7 +1,12 @@
 package com.example.officialgsmclientserver.domain.board.repository;
 
+import com.example.officialgsmclientserver.domain.board.entity.post.Category;
 import com.example.officialgsmclientserver.domain.board.entity.post.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Page<Post> findAllByCategory(Pageable pageable, Category category);
 }
