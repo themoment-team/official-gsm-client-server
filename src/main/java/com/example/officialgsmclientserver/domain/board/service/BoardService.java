b2c2d7b43ee7724d81387c97e0eb3884bcf60e10
jangwooooo <s22018@gsm.hs.kr>
2023-04-15 20:07:03 +0900

    Add BoardService

---
+++ src/main/java/com/example/officialgsmclientserver/domain/board/service/BoardService.java
@@ -0,0 +1,27 @@
+package com.example.officialgsmclientserver.domain.board.service;
+
+import com.example.officialgsmclientserver.domain.board.dto.response.PostListResponse;
+import com.example.officialgsmclientserver.domain.board.entity.post.Category;
+import com.example.officialgsmclientserver.domain.board.entity.post.Post;
+import com.example.officialgsmclientserver.domain.board.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final PostRepository postRepository;
+
+    public Page<PostListResponse> findPosts(int pageNumber, Category category) {
+        Pageable pageable = PageRequest.of(pageNumber, 5, Sort.by("createdAt").descending());   // pageSize는 추후 수정
+        Page<Post> posts = postRepository.findAllByCategory(pageable, category);
+
+        return posts.map(PostListResponse::from);
+    }
+
+}
