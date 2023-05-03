18d2d1064ee52aae2a4eb5e3e6518ade9782e400
jangwooooo <s22018@gsm.hs.kr>
2023-05-03 09:40:25 +0900

    Add boardController api mapping

--- src/main/java/com/example/officialgsmclientserver/domain/board/controller/BoardController.java
+++ src/main/java/com/example/officialgsmclientserver/domain/board/controller/BoardController.java
@@ -8,12 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/post")
 @RequiredArgsConstructor
 public class BoardController {
 
@@ -25,7 +23,7 @@ public class BoardController {
         return new ResponseEntity<>(postList, HttpStatus.OK);
     }
 
-    @GetMapping
+    @GetMapping("/{postSeq}")
     public ResponseEntity<PostDetailResponse> postDetail(@PathVariable Long postSeq) {
         PostDetailResponse postDetail = boardService.findPost(postSeq);
         return new ResponseEntity<>(postDetail, HttpStatus.OK);
