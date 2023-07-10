fc5175d64b50299da6a9d778e899ef06ad8bfc6c
Junho-06 <k34920032@gmail.com>
2023-07-10 14:23:29 +0900

    update docker build arm based

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ec2-user/official-gsm-client-server
 
 cd $REPOSITORY
 
-docker build -t official-test-server:client .
+docker build --platform linux/arm64 -t official-test-server:client .
 
 docker-compose up -d
 
