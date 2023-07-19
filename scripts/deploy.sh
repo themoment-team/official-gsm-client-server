b9b391c0e8a6636e11a3a9ab13a740995d229e68
Junho-06 <k34920032@gmail.com>
2023-07-20 08:34:07 +0900

    update file mod

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,12 @@ DEPLOYMENT_GROUP_NAME=$(echo $DEPLOYMENT_GROUP_NAME)
 if [ "$DEPLOYMENT_GROUP_NAME" == "official-client-test-server" ]; then
   # official-client-test-server 배포 그룹에 대한 스크립트 실행
   echo "Running dev-deploy.sh..."
+  chmod +x /home/ec2-user/official-gsm-client-server/scripts/dev-deploy.sh
   /home/ec2-user/official-gsm-client-server/scripts/dev-deploy.sh
 elif [ "$DEPLOYMENT_GROUP_NAME" == "official-prod-client-server" ]; then
   # official-prod-client-server 배포 그룹에 대한 스크립트 실행
   echo "Running prod-deploy.sh..."
+  chmod +x /home/ec2-user/official-gsm-client-server/scripts/prod-deploy.sh
   /home/ec2-user/official-gsm-client-server/scripts/prod-deploy.sh
 else
   echo "Unknown deployment group: $DEPLOYMENT_GROUP_NAME"
